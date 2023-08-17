230e355ebb0b4f97f642106f5a78f69026cf55aa
yogesh1483 <yogeshingole1483@gmail.com>
2023-08-17 11:51:02 +0530

    1st commkit

---
+++ LoanApplicationProject/src/main/java/com/loanapp/main/LoanApplicationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.loanapp.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanApplicationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanApplicationProjectApplication.class, args);
+	}
+
+}
