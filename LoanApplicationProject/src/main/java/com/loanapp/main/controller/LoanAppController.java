7244a83463b11b7199b5775db7d3f783569fe725
yogesh1483 <yogeshingole1483@gmail.com>
2023-08-18 11:52:38 +0530

    Admin Save

--- LoanApplicationProject/src/main/java/com/loanapp/main/controller/LoanAppController.java
+++ LoanApplicationProject/src/main/java/com/loanapp/main/controller/LoanAppController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +12,6 @@ import org.springframework.web.multipart.MultipartFile;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.loanapp.main.entity.Admin;
-import com.loanapp.main.entity.BaseResponse;
 import com.loanapp.main.servicei.LoanAppServiceI;
 
 
