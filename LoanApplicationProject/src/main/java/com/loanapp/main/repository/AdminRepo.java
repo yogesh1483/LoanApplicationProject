f0dc52c13e3dc14f88be98da93c6f999bcf8b908
yogesh1483 <yogeshingole1483@gmail.com>
2023-08-18 12:45:40 +0530

    Admin Login with Username and Password API

--- LoanApplicationProject/src/main/java/com/loanapp/main/repository/AdminRepo.java
+++ LoanApplicationProject/src/main/java/com/loanapp/main/repository/AdminRepo.java
@@ -1,9 +1,13 @@
 package com.loanapp.main.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.loanapp.main.entity.Admin;
 
 public interface AdminRepo extends JpaRepository<Admin, Integer> {
+
+	List<Admin> findAllByAusernameAndApassword(String ausername, String apassword);
 
 }
