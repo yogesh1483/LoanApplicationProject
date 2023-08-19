2d46b277593ece4fc4ab03ccfff69541905f9353
supriyaghuge123 <supriyaghuge1993@gmail.com>
2023-08-19 12:39:30 +0530

    User Creation Backend API

--- LoanApplicationProject/src/main/java/com/loanapp/main/servicei/LoanAppServiceI.java
+++ LoanApplicationProject/src/main/java/com/loanapp/main/servicei/LoanAppServiceI.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.loanapp.main.entity.Admin;
+import com.loanapp.main.entity.Users;
+import com.loanapp.main.exception.UserCanNotCreatedException;
 import com.loanapp.main.exception.UserNotFoundException;
 
 public interface LoanAppServiceI {
@@ -12,5 +14,7 @@ public interface LoanAppServiceI {
 	Admin saveAdmin(Admin admin, MultipartFile profileimg);
 
 	List<Admin> adminLogin(String ausername, String apassword) throws UserNotFoundException;
+
+	Users saveUser(Users user, MultipartFile profileImg)throws UserCanNotCreatedException;
 
 }
--- LoanApplicationProject/src/main/java/com/loanapp/main/serviceimpl/LoanAppServiceImpl.java
+++ LoanApplicationProject/src/main/java/com/loanapp/main/serviceimpl/LoanAppServiceImpl.java
@@ -3,21 +3,29 @@ package com.loanapp.main.serviceimpl;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.loanapp.main.entity.Admin;
+import com.loanapp.main.entity.Users;
+import com.loanapp.main.exception.UserCanNotCreatedException;
 import com.loanapp.main.exception.UserNotFoundException;
 import com.loanapp.main.repository.AdminRepo;
+import com.loanapp.main.repository.UsersRepo;
 import com.loanapp.main.servicei.LoanAppServiceI;
 
 @Service
 public class LoanAppServiceImpl implements LoanAppServiceI {
 
 	@Autowired
+	Random rm;
+	@Autowired
 	AdminRepo ar;
+	@Autowired
+	UsersRepo ur;
 	
 	@Override
 	public Admin saveAdmin(Admin admin, MultipartFile profileimg) {
@@ -41,6 +49,26 @@ public class LoanAppServiceImpl implements LoanAppServiceI {
 		else{
 			throw new UserNotFoundException("No User Found in Database:- "+ausername);
 		}
+	}
+
+	@Override
+	public Users saveUser(Users user, MultipartFile profileImg) throws UserCanNotCreatedException {
+		if(user!=null) {
+		try {
+			user.setProfileImg(profileImg.getBytes());
+			user.setUserName(user.getName()+""+user.getUserType()+"@"+rm.nextInt(4444));
+			user.setUserPassword(user.getUserType()+"@"+rm.nextInt(1111));
+			return ur.save(user);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		}else
+		{
+			throw new UserCanNotCreatedException("User Can Not Be Created Provide Valid Information");
+		}
+		
+		return null;
 	}
 
 }
