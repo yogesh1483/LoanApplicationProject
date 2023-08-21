fe2506e2c3d88937a10e0bbeb138fdcfb137dd43
yogesh1483 <yogeshingole1483@gmail.com>
2023-08-21 11:59:42 +0530

    Unused Data Removed

--- LoanApplicationProject/src/main/java/com/loanapp/main/servicei/LoanAppServiceI.java
+++ LoanApplicationProject/src/main/java/com/loanapp/main/servicei/LoanAppServiceI.java
@@ -3,18 +3,12 @@ package com.loanapp.main.servicei;
 import java.util.List;
 
 import org.springframework.web.multipart.MultipartFile;
-
-import com.loanapp.main.entity.Admin;
 import com.loanapp.main.entity.EnquiryDetails;
 import com.loanapp.main.entity.Users;
 import com.loanapp.main.exception.UserCanNotCreatedException;
 import com.loanapp.main.exception.UserNotFoundException;
 
 public interface LoanAppServiceI {
-
-	Admin saveAdmin(Admin admin, MultipartFile profileimg);
-
-	List<Admin> adminLogin(String ausername, String apassword) throws UserNotFoundException;
 
 	Users saveUser(Users user, MultipartFile profileImg)throws UserCanNotCreatedException;
 
