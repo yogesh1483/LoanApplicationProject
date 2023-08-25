542d1659ea1b36bb1807bc655426f3557576a287
Maddy108 <sonwanemadhusudan.ms@gmail.com>
2023-08-24 12:32:07 -1200

    FOR YOGESH

--- GetCibilScore/src/main/java/com/cibilscore/main/controller/GetCibilScoreController.java
+++ GetCibilScore/src/main/java/com/cibilscore/main/controller/GetCibilScoreController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GetCibilScoreController {
 	Random random = new Random();
+	//for getting cibil score
 	@GetMapping("/getCibilScore/{pancardNumber}")
 	public int getCibilScore(@PathVariable("pancardNumber") String pancardNumber) {
 		if(pancardNumber.length()==12) {
